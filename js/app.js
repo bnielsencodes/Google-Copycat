c0f80094fb0d71d287d65b72b5f4fdf565b46cbc
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-06-25 19:54:54 -0700

    add multiple things
    add images for customize modal grid
    
    add js for top border of .modal-center-container
    
    add html and styling for mini-google-page
    
    add some setup styling for a light/dark mode toggle to implement later
    
    adjust styling to modal grid

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@ const search = document.getElementById("search-input");
 const dropdownHide = document.getElementById("dropdown-hide");
 const dropdown = document.querySelector(".dropdown");
 const searchBar = document.querySelector(".search-bar");
+const modalGrid = document.getElementById("modal-grid");
+const topScrollBorder = document.querySelector(".top-scroll-border");
+
+
+/* ----------------- SEARCH DROPDOWN LIST ----------------- */
 
 search.addEventListener("click", () => {
   dropdown.classList.toggle("show");
@@ -10,9 +15,39 @@ search.addEventListener("click", () => {
   search.classList.toggle("search-padding-top");
 });
 
+/* ----------------- MODAL HIDE/SHOW ----------------- */
 
 
 
+
+
+
+
+
+/* ----------------- TOGGLE MODAL CENTER CONTAINER TOP BORDER ----------------- */
+
+modalGrid.onscroll = function () {
+  myFunction()
+};
+
+function myFunction() {
+  if (modalGrid.scrollTop > 0) {
+    topScrollBorder.className = "top-scroll-border show-2";
+  } else {
+    topScrollBorder.className = "top-scroll-border";
+  }
+};
+
+
+
+
+
+
+
+
+
+
+/* ---------------- x dropdown list ---------------*/
 
 // const searchX = document.querySelectorAll(".search-x");
 
